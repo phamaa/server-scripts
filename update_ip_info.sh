c1ea1766b7a2bcbd0b6ebecf5d9cfa81fa6f8eee
Quang <me@quangpham.com>
2013-02-28 19:08:00 +0200

    .

--- update_ip_info.sh
+++ update_ip_info.sh
@@ -1,5 +1,5 @@
 curl http://ipecho.net/plain -o /tmp/internet_ip.info
 ifconfig > /tmp/local_ip.info
 
-/home/quang/scripts/dropbox_uploader.sh /tmp/internet_ip.info /tmp/internet_ip.info
-/home/quang/scripts/dropbox_uploader.sh /tmp/local_ip.info /tmp/local_ip.info
+/home/quang/scripts/dropbox_uploader.sh upload /tmp/internet_ip.info /tmp/internet_ip.info
+/home/quang/scripts/dropbox_uploader.sh upload /tmp/local_ip.info /tmp/local_ip.info
