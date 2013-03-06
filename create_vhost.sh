6adc7b2564bda00d2da800e96dc87152c27def13
Quang Pham <me@quangpham.com>
2013-03-06 15:45:48 +0200

    .

--- create_vhost.sh
+++ create_vhost.sh
@@ -5,3 +5,5 @@ sudo sh -c 'echo "   ServerName $1" >> /etc/apache2/sites-available/$1'
 sudo sh -c 'echo "   ErrorLog /home/quang/public_html/$1/logs/error.log" >> /etc/apache2/sites-available/$1'
 sudo sh -c 'echo "   CustomLog /home/quang/public_html/$1/logs/access.log combined" >> /etc/apache2/sites-available/$1'
 sudo sh -c 'echo "</VirtualHost>" >> /etc/apache2/sites-available/$1'
+
+a2dissite $1 && a2ensite $1 && service apache2 reload
