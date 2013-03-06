9a0f996ab90fef5c219718efbdc56d80be63f8bb
Quang Pham <me@quangpham.com>
2013-03-06 15:44:53 +0200

    .

--- apache_common.sh
+++ apache_common.sh
@@ -3,13 +3,21 @@ sudo sh -c 'echo "ServerName localhost" >> /etc/apache2/conf.d/name'
 
 #enable dump_io
 sudo a2enmod dump_io
+sudo sh -c 'echo "#" >> /etc/apache2/apache2.conf'
+sudo sh -c 'echo "# Quang" >> /etc/apache2/apache2.conf'
+sudo sh -c 'echo "# enable this if you just want to capture http request" >> /etc/apache2/apache2.conf'
 sudo sh -c 'echo "DumpIOInput On" >> /etc/apache2/apache2.conf'
+sudo sh -c 'echo "# enable this if you just want to capture http response" >> /etc/apache2/apache2.conf'
 sudo sh -c 'echo "DumpIOOutput On" >> /etc/apache2/apache2.conf'
+sudo sh -c 'echo "# capture everything" >> /etc/apache2/apache2.conf'
 sudo sh -c 'echo "DumpIOLogLevel debug" >> /etc/apache2/apache2.conf'
+
 
 #enable rewrite
 sudo a2enmod rewrite
 
 #install uploadprogress
 sudo pecl install uploadprogress
+sudo sh -c 'echo "#" >> /etc/php5/apache2/php.ini'
+sudo sh -c 'echo "# Quang install uploadprogress" >> /etc/php5/apache2/php.ini'
 sudo sh -c 'echo "extension=uploadprogress.so" >> /etc/php5/apache2/php.ini'
---
+++ create_vhost.sh
@@ -0,0 +1,7 @@
+mkdir /home/quang/public_html/$1
+sudo sh -c 'echo "<VirtualHost *:80>" >> /etc/apache2/sites-available/$1'
+sudo sh -c 'echo "   DocumentRoot /home/quang/public_html/$1" >> /etc/apache2/sites-available/$1'
+sudo sh -c 'echo "   ServerName $1" >> /etc/apache2/sites-available/$1'
+sudo sh -c 'echo "   ErrorLog /home/quang/public_html/$1/logs/error.log" >> /etc/apache2/sites-available/$1'
+sudo sh -c 'echo "   CustomLog /home/quang/public_html/$1/logs/access.log combined" >> /etc/apache2/sites-available/$1'
+sudo sh -c 'echo "</VirtualHost>" >> /etc/apache2/sites-available/$1'
