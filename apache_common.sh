57ef30fed22cd37ea764f68720d87dacf112534a
Quang Pham <me@quangpham.com>
2013-03-06 15:30:59 +0200

    .

--- apache_common.sh
+++ apache_common.sh
@@ -6,3 +6,10 @@ sudo a2enmod dump_io
 sudo sh -c 'echo "DumpIOInput On" >> /etc/apache2/apache2.conf'
 sudo sh -c 'echo "DumpIOOutput On" >> /etc/apache2/apache2.conf'
 sudo sh -c 'echo "DumpIOLogLevel debug" >> /etc/apache2/apache2.conf'
+
+#enable rewrite
+sudo a2enmod rewrite
+
+#install uploadprogress
+sudo pecl install uploadprogress
+sudo sh -c 'echo "extension=uploadprogress.so" >> /etc/php5/apache2/php.ini'
